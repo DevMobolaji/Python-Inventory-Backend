1e27e01e17540bb7496330affb7672c50083c843
Tech <alan08037896270@outlook.com>
2023-02-27 00:11:15 +0100

    made some few changes to our repository

--- authentication/views.py
+++ authentication/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from rest_framework import generics, status, views
 from .serializers import EmailVerificationSerializer, RegisterSerializer, LoginSerializer
 from rest_framework.response import Response
