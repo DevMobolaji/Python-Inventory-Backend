dae3883b1ea7c5ae88b2da6d7897d81f89bcc708
BeejayDev <alan08037896270@outlook.com>
2023-04-14 00:02:57 +0100

    Fixed bugs in requirements file

--- python_Backend_API/settings.py
+++ python_Backend_API/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = "django-insecure--z^l^jgleiwm_f0u3&&h95yuj4amo52mo4e4c=7n8i@4(=#ok("
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
